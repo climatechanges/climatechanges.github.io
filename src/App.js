6e12be82de8ed22c032ee3771dcdf12cddc26f53
climatechanges <datavisclimatechange@gmail.com>
2024-11-25 06:28:47 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,57 @@
+import './App.css';
+import React from 'react';
+import TableauEmbed from './TableauEmbed';
+
+function App() {
+  const sumOfExposureValues = 'https://public.tableau.com/views/SumExposureMap/Dashboard12?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+  const exposureValues2022 = 'https://public.tableau.com/views/ExposureValues2022/ExposureValues?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+  const yearlyTempAnomolies = 'https://public.tableau.com/views/BasicTemperatureVis/AreaChart?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+  const monthlyTempAnomolies = 'https://public.tableau.com/views/UniqueTemperatureVis/Heatmap?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+  const magnitudeOfTempAnomoliesByMonth = 'https://public.tableau.com/views/UniqueTemperatureVis/RadialChart?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link ';
+  const nationalAndStateRiskLevel = 'https://public.tableau.com/shared/4NHP76KDY?:display_count=n&:origin=viz_share_link ';
+  const precipitationAndTempChangesbyState = 'https://public.tableau.com/views/BetterMaps_17324144202350/ScatterPlotbyChanges?:language=en-US&publish=yes&:sid=&:redirect=auth&:display_count=n&:origin=viz_share_link';
+
+  return (
+    <div className="App">
+      <p>The effects of climate change can take on a variety forms: rising temperature, changing precipitation patterns, more extreme weather like floods and storms. One way to understand the impact of these effects is to look at the percentage of population or land exposed.</p>
+      <TableauEmbed url={sumOfExposureValues}/>
+      <br/>
+
+      <p>Only one country had no exposure to any of the measured events in 2022:  Tokelau.
+On average, nations experienced 4.5 out of 9 of the measured events, revealing the vast and relentless reach of this crisis. 
+
+This stark reality shows that no nation, no matter its size or location, is untouched by our changing world. The biggest concerns may differ from region to region, but one thing is clear: climate change is a global emergency, and the danger is everywhere.
+
+Click on the + under Continent to see the measures at a country level. (maybe we could put this next to where they should click)
+</p>
+      <TableauEmbed url={exposureValues2022}/>
+      <br/>
+
+      <p>The following graph shows the magnitude of the anomaly for each year compared to the baseline years of 1951-1980. For example, in 1947 the anomaly was .460 which means that 1947 was .46 degrees Celsius warmer than in 1951-1980. As can be seen, nearly every year before 1951 was cooler than the baseline of 1951-1980 (with the exception of 1940-1945 which was during World War 2 leading to a change and inconsistency of measurement) and every year after 1980 was warmer than the baseline. In fact, 2023 had an anomaly of 1.170. According to Mapfre, a single degree change can lead to rising sea levels, loss of arctic ice, extreme heat waves, and force changes in agricultural practices (due to temperatures changing in agricultural regions). A continuing increase of temperature could lead to rapid changes in participation, temperature, and extreme weather events. 
+      </p>
+      <TableauEmbed url={yearlyTempAnomolies}/>
+      <br/>
+
+      <p>This graph shows the same data while adding information about the months. As is seen, the anomaly has continued to increase from the baseline.</p>
+      <TableauEmbed url={monthlyTempAnomolies}/>
+      <br/>
+
+      <p>The following chart illustrates the magnitude of the anomaly per month. The color code here is the years with lighter colors being earlier years and darker colors being later years. Not only are the more recent years showing a greater anomaly (farther from the center of the chart), they are getting increasingly spaced out (the outer spokes have larger distances than earlier years). This shows that not only is temperature increasing, but it is increasing at a faster rate than earlier years.</p>
+      <TableauEmbed url={magnitudeOfTempAnomoliesByMonth}/>
+      <br/>
+
+      <p>The following map shows the anticipated climate risk level for each county in 2040-2049. The risk is made up of the amount of hazard, exposure, and vulnerability in each county. Click on a county to learn more about their specific risk as well as state-wide risk.</p>
+      <TableauEmbed url={nationalAndStateRiskLevel}/>
+      <br/>
+
+      <p>This scatter plot shows anticipated temperature and precipitation changes in 2040-2049 for each state. Click on an item to learn more about state-risks.</p>
+      <TableauEmbed url={precipitationAndTempChangesbyState}/>
+      <br/>
+      <br/>
+      <p>WOAH Guy's super cool oxygen stuff is going to go here</p>
+      <p>Maybe some really cool rain graphs might go here, idk</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TableauEmbed.js
@@ -0,0 +1,17 @@
+import React, { useEffect, useRef } from 'react';
+
+const TableauEmbed = ({ url }) => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const { tableau } = window;
+    const viz = new tableau.Viz(ref.current, url);
+    return () => {
+      viz.dispose();
+    };
+  }, [url]);
+
+  return <div ref={ref} style={{ width: '50%', height: '600px' }}></div>;
+};
+
+export default TableauEmbed;
