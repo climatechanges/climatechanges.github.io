48265dbaf479f1061d4977d504b69b3eab2ae1e9
superpatrex <jack@ding911.com>
2024-12-01 22:32:30 -0500

    Changed npm and made lazy loading of embeddings

--- src/TableauEmbed.js
+++ src/TableauEmbed.js
@@ -1,15 +1,40 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const TableauEmbed = ({ url }) => {
   const ref = useRef(null);
+  const [isInView, setIsInView] = useState(false);
 
   useEffect(() => {
-    const { tableau } = window;
-    const viz = new tableau.Viz(ref.current, url);
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsInView(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
     return () => {
-      viz.dispose();
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
     };
-  }, [url]);
+  }, []);
+
+  useEffect(() => {
+    if (isInView) {
+      const { tableau } = window;
+      const viz = new tableau.Viz(ref.current, url);
+      return () => {
+        viz.dispose();
+      };
+    }
+  }, [isInView, url]);
 
   return <div className="tableauEmbed" ref={ref} style={{ width: '100%', height: '600px' }}></div>;
 };
